45661b8baee64c92085ec39513bd772df5a99734
Gloomsfield <gloomsfield@gmail.com>
2025-02-13 21:16:05 -0800

    Add boilerplate for reading source files

--- src/main.zig
+++ src/main.zig
@@ -4,28 +4,93 @@ const sdl = @cImport({
     @cInclude("SDL3/sdl.h");
 });
 
-var window: *sdl.SDL_Window = undefined;
+const MAX_FILENAME_SIZE: comptime_int = 1024;
+const MAX_SOURCE_SIZE: comptime_int = 1024;
+
+pub fn read_user_input(
+    allocator: std.mem.Allocator,
+    max_size: comptime_int
+) ![]const u8 {
+    const stdin = std.io.getStdIn().reader();
+        
+    const raw_input = try stdin.readUntilDelimiterAlloc(
+        allocator,
+        '\n',
+        max_size,
+    );
+
+    return std.mem.trim(u8, raw_input, "\r");
+}
+
+fn read_source_file(
+    allocator: std.mem.Allocator,
+    file_path: []const u8,
+    max_size: comptime_int,
+) ![]const u8 {
+    return std.fs.cwd().readFileAlloc(
+        allocator,
+        file_path,
+        max_size,
+    );
+}
 
 pub fn main() !void {
-    window = sdl.SDL_CreateWindow(
-        "meow!",
-        640,
-        480,
-        sdl.SDL_WINDOW_OPENGL
-    ) orelse @panic("failed to create SDL window...");
+    const stdout = std.io.getStdOut().writer();
+    
+    try std.fmt.format(
+        stdout,
+        "you're face-to-face with an old training dummy. please input a move!!\n",
+        .{ }
+    );
+
+    var arena_allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+
+    const allocator = arena_allocator.allocator();
 
     var quit: bool = false;
-    var event: sdl.SDL_Event = undefined;
 
     while(!quit) {
-        while(sdl.SDL_PollEvent(&event) != false) {
-            if(event.type == sdl.SDL_EVENT_QUIT) {
-                quit = true;
-            }
-        }
-    }
+        defer arena_allocator.deinit();
 
-    sdl.SDL_DestroyWindow(window);
-    sdl.SDL_Quit();
+        const user_input = read_user_input(
+            allocator,
+            MAX_FILENAME_SIZE
+        ) catch |err| {
+            try std.fmt.format(
+                stdout,
+                "error when getting user input: {s}\n",
+                .{ @errorName(err) }
+            );
+
+            continue;
+        };
+
+        if(std.mem.eql(u8, user_input, "q")) {
+            quit = true;
+            continue;
+        }
+
+        const file_path = user_input;
+
+        const source_contents = read_source_file(
+            allocator,
+            file_path,
+            MAX_SOURCE_SIZE
+        ) catch |err| {
+            try std.fmt.format(
+                stdout,
+                "error when attempting to read file \"{s}\": {s}\n",
+                .{ file_path, @errorName(err) }
+            );
+
+            continue;
+        };
+
+        try std.fmt.format(
+            stdout,
+            "code:\n{s}\n",
+            .{ source_contents }
+        );
+    }
 }
 
